635d72124acf1c4741525dd09f715e957dab85a8
Darth <abdulrubbaig@gmail.com>
2024-08-04 00:14:33 +0500

    ok

---
+++ NextJS/app1-pnpm/my-app/src/app/(BlogRoute)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layoutSettings({children}:{children:React.ReactNode}) {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default layoutSettings
---
+++ NextJS/app1-pnpm/my-app/src/constants/index.ts
@@ -0,0 +1,31 @@
+export const blogs = [ {
+    id: 1,
+    title: "Blog 1",
+    content: "This is the content of blog 1",
+    author: "John Doe",
+  },
+  {
+    id: 2,
+    title: "Blog 2",
+    content: "This is the content of blog 2",
+    author: "John Doe",
+  },
+  {
+    id: 3,
+    title: "Blog 3",
+    content: "This is the content of blog 3",
+    author: "John Doe",
+  },
+  {
+    id: 4,
+    title: "Blog 4",
+    content: "This is the content of blog 4",
+    author: "John Doe",
+  },
+  {
+    id: 5,
+    title: "Blog 5",
+    content: "This is the content of blog 5",
+    author: "John Doe",
+  },
+]
