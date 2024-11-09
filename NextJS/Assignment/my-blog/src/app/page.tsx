f8793e71a0ccd16bbbbf833b098894ec0dd96986
beyg1 <abdulrubbaig@gmail.com>
2024-11-09 18:42:09 +0500

    gemini suggested ui for blog

--- NextJS/Assignment/my-blog/src/app/page.tsx
+++ NextJS/Assignment/my-blog/src/app/page.tsx
@@ -8,36 +8,37 @@ import Node from '../components/logos/nodejs.svg' // Importing Nodejs logo
 import TailwindL from '../components/logos/tailwind.svg' // Importing Tailwind logo
 import TypescriptL from '../components/logos/typescript.svg' // Importing Typescript logo
 import Logo from '../components/logos/html5.svg' // Importing Html Logo
+import Illustration from '../components/illustrations/illustration.svg' // Importing Illustration
 
 
 const fontA = Raleway({
-  weight: ['400',],
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
 })
 const fontB = Roboto_Mono({
-  weight: ['400',],
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
 })
 
 export default function Home() { 
     return (
-      <div className={`bg-[#fcd86b] min-h-screen text-2xl`}>         
+      <div className={`bg-[#e0f2f7] min-h-screen text-2xl`}>         
         <main className="container mx-auto p-4 sm:p-8">
           <section className="mb-8">    
             <div className="flex flex-col md:flex-row items-start mb-4">
               <div className="w-full md:w-2/3 pr-0 md:pr-8">
-                <div className={`${fontA.className} text-xl md:text-2xl text-white font-medium text-justify mb-4`}>
-                 <h2 className='motion-preset-pop motion-duration-500 motion-delay-150'> Hey there! I'm Volk – a code-slinging Software Engineer, who likes to
+                <div className={`${fontA.className} text-justify mb-4`}>
+                 <h2 className='text-black font-medium motion-preset-pop motion-duration-500 motion-delay-150'> Hey there! I'm Volk – a code-slinging Software Engineer, who likes to
                   dive in the market charts as I am debugging production systems..
                  </h2>
                 </div>
-                <p className={`${fontB.className} text-black font-normal text-xl md:text-2xl text-justify`}>
+                <p className={`${fontB.className}  text-[#232946] text-justify`}>
                   During the day, I craft digital solutions that make the businesses tick. By night, I'm analyzing market trends, hunting
                   for the next big tech opportunity, or planning my next adventure across the globe. Yeah, I'm that guy who gets equally 
                   excited about a perfectly executed trading strategy and a beautifully written piece of code.
                 </p> <br />
-                <p className='text-white motion-preset-pop motion-duration-500 motion-delay-350'>Here's what to expect from this blog:</p>  
-                <p className={`${fontB.className} text-xl md:text-2xl text-black`}>
+                <p className={`${fontA.className} text-black font-medium motion-preset-pop motion-duration-500 motion-delay-350`}>Here's what to expect from this blog:</p>  
+                <p className={`${fontB.className} text-[#232946]`}>
                 --Tales from the trenches of software development <br />
                 --My trading adventures (wins and face-palms included) <br />
                 --Tech deep-dives that won't put you to sleep <br />
@@ -48,31 +49,33 @@ export default function Home() {
               <div className="flex flex-col items-center md:items-center mt-8 lg:ml-12"> {/* Adjusted alignment for larger screens */}
                 <Image src='/pfp/1.jpg' alt="Profile Picture" width={450} height={300} className="rounded-full motion-preset-bounce motion-duration-750 motion-delay-500" />
                 <div className="flex flex-wrap justify-center md:justify-end gap-4 mt-8 mx-4 md:mx-20"> {/* Responsive logo container */}
-                  <Image src={C} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-75'/> 
-                  <Image src={Python} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-125'/>
-                  <Image src={Nextjs} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-150'/>
-                  <Image src={ReactIcon} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-175'/>
+                  <Image src={C} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-75 hover:scale-150 transition-transform duration-300 ease-in-out' /> 
+                  <Image src={Python} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-125 hover:scale-150 transition-transform duration-300 ease-in-out' />
+                  <Image src={Nextjs} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-150 hover:scale-150 transition-transform duration-300 ease-in-out' />
+                  <Image src={ReactIcon} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-175 hover:scale-150 transition-transform duration-300 ease-in-out' />
                 </div>
                 <div className={`${fontA.className} flex flex-wrap justify-center md:justify-end gap-4 mt-10 mx-4 md:mx-20`}> {/* Responsive logo container */}
-                  <Image src={Node} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-225'/> 
-                  <Image src={TailwindL} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-100'/>
-                  <Image src={TypescriptL} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-50'/>
-                  <Image src={Logo} alt="logo" width={40} height={40} className='motion-preset-oscillate motion-duration-500 motion-delay-300'/>
+                  <Image src={Node} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-225 hover:scale-150 transition-transform duration-300 ease-in-out' /> 
+                  <Image src={TailwindL} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-100 hover:scale-150 transition-transform duration-300 ease-in-out' />
+                  <Image src={TypescriptL} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-50 hover:scale-150 transition-transform duration-300 ease-in-out' />
+                  <Image src={Logo} alt="logo" width={50} height={50} className='motion-preset-oscillate motion-duration-500 motion-delay-300 hover:scale-150 transition-transform duration-300 ease-in-out' />
                 </div>
               </div>                           
             </div>                                  
-            <p className='w-full md:w-2/3 text-white motion-preset-pop motion-duration-500 motion-delay-200'>Ready for some unique perspective? Let's explore where technology meets money then embraces 
+            <p className={`${fontA.className} w-full md:w-2/3 text-black font-medium motion-preset-pop motion-duration-500 motion-delay-200`}>Ready for some unique perspective? Let's explore where technology meets money then embraces 
                wanderlust! 🚀</p>            
           </section>
           <section>
-            <h2 className="text-xl md:text-2xl font-semibold mb-4 text-black">Recent Posts</h2>
+            <h2 className={`${fontA.className} text-xl md:text-2xl mb-4 text-[#232946]`}>Recent Posts</h2>
             {/* Placeholder for recent posts */}
-            <p className="text-white">
+            <p className={`${fontB.className} text-black `}>
               (Coming soon!)
             </p>
+          </section>
+          <section className='flex justify-end mt-10'>
+            <Image src={Illustration} alt="Illustration" width={500} height={300} className='motion-preset-slide-down motion-duration-550 motion-delay-500' />
           </section>
         </main>         
       </div>
     );
   }
-  
--- NextJS/Assignment/my-blog/src/components/header/Header.tsx
+++ NextJS/Assignment/my-blog/src/components/header/Header.tsx
@@ -9,7 +9,7 @@ const font = Nunito({
 
 const Header: React.FC = () => {
   return (
-    <header className={`text-black font-semibold text-3xl ${font.className} bg-[#fcd86b] p-2 flex flex-col md:flex-row justify-between items-center`}>
+    <header className={`text-black font-semibold text-3xl ${font.className} bg-[#e0f2f7] p-2 flex flex-col md:flex-row justify-between items-center`}>
       <button className="transform hover:scale-105 motion-preset-slide-right motion-duration-500 motion-delay-500 mb-2 md:mb-0">
         Volk's Personal Blog
       </button>
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
         <button className="transform hover:scale-110 motion-preset-compress motion-delay-150 motion-duration-500 py-2 px-4 rounded-md">
           Traveling Blogs
         </button>
-        <button className="transform hover:scale-110 motion-preset-compress motion-delay-200 motion-duration-500 py-2 px-4 rounded-md">
+        <button className="transform hover:scale-105 motion-preset-compress motion-delay-200 motion-duration-500 py-2 px-4 rounded-md">
           Financial Blogs
         </button>
       </div>
