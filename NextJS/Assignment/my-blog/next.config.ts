a56d0cc8781303c0977e30aa4bf2150c186a4f4c
beyg1 <abdulrubbaig@gmail.com>
2024-11-12 00:46:03 +0500

    ok

--- NextJS/Assignment/my-blog/next.config.ts
+++ NextJS/Assignment/my-blog/next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
