c2d34ffaedd37f25c7e7273fd2b12ca6800c0afa
beyg1 <abdulrubbaig@gmail.com>
2024-11-12 12:07:55 +0500

    blog build

--- NextJS/Assignment/my-blog/src/app/[slug]/page.tsx
+++ NextJS/Assignment/my-blog/src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { PortableText, type SanityDocument } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import  client  from "../../sanity/lib/client";
+import Image from "next/image";
 
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
@@ -17,7 +18,7 @@ const options = { next: { revalidate: 30 } };
 export default async function PostPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
   const post = await client.fetch<SanityDocument>(POST_QUERY, params, options);
   const postImageUrl = post.mainImage
@@ -27,12 +28,15 @@ export default async function PostPage({
   return (    
     <main className="mx-auto gap-5 bg-[#c6b291] min-h-screen flex flex-col">
       {postImageUrl && (
-        <img
-          src={postImageUrl}
-          alt={post.title}
-          className="mx-auto my-4 rounded-3xl"
-          style={{ width: '600px', height: '480px' }}
-        />
+        <Image
+        src={postImageUrl}
+        alt={post.title}
+        width={600} // Explicitly set the width
+        height={480} // Explicitly set the height
+        className="mx-auto my-4 rounded-3xl"
+        style={{ width: '600px', height: '480px' }} // Optional: keep for styling if needed
+      />
+      
       )}
       <h1 className="text-4xl p-4 gap-4 mx-auto font-bold mb-8">{post.title}</h1>
       <div className="mx-4 md:mx-28 text-justify text-2xl">
