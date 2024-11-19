c233997edaeb41bad35e3dd84a3cb615f9aa618b
beyg1 <abdulrubbaig@gmail.com>
2024-11-19 13:55:49 +0500

    ok

--- NextJS/Assignment/my-blog/sanity.config.ts
+++ NextJS/Assignment/my-blog/sanity.config.ts
@@ -7,7 +7,6 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import { deskTool } from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
@@ -24,8 +23,7 @@ export default defineConfig({
   apiVersion: '2024-01-24',
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
-  plugins: [
-    deskTool(),
+  plugins: [    
     structureTool({structure}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
